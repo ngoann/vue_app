5ad912c4908d0fac28a86c915c9ea7691e6a25fe
ngoann <wapa2pro@gmail.com>
2018-08-26 19:32:02 +0700

    Init app

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button type="button" name="button">{{ msg }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  data () {
+    return {
+      msg: 'button name'
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>sdfsd</div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
